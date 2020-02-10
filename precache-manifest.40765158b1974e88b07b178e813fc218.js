ca1ef0510533fb1ff0a785942bbb2cf32d607618
Francisco-Castro <zipcodeFrancisco@gmail.com>
2020-02-10 13:23:14 -0500

    Updates

---
+++ precache-manifest.40765158b1974e88b07b178e813fc218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b067920eaf11851dffbbe3e5af87b5d",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "4de163a884445fa467c1",
+    "url": "/webpage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7f026176c23b82e37fc",
+    "url": "/webpage/static/js/2.5cbaf944.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/webpage/static/js/2.5cbaf944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de163a884445fa467c1",
+    "url": "/webpage/static/js/main.e68ccd43.chunk.js"
+  },
+  {
+    "revision": "115d0da617ff51e2de80",
+    "url": "/webpage/static/js/runtime-main.39f6bd7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
